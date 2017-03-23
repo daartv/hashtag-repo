4eebcd979167cc959fc6e09cb111222d44f3c270
Mycah Jay Anast <mycahjay@gmail.com>
2017-03-23 16:45:17 -0700

    Add bill template created

---
+++ client/src/containers/AddBill.js
@@ -0,0 +1,39 @@
+// TEMPLATE
+
+import React from 'react'
+import { Form, FormGroup, InputGroup, FormControl, Button, Col } from 'react-bootstrap'
+
+const AddBill = (props) => {
+  return (
+      <Form horizontal>
+        <FormGroup controlId='addFriendName'>
+          <Col md={4}>
+            <FormControl type='text' placeholder="Friend's name" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId='addFriendEmail'>
+          <Col md={4}>
+            <FormControl type='text' placeholder="Friend's email" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId='addFriendAmount'>
+          <Col md={4}>
+            <InputGroup>
+              <InputGroup.Addon>$</InputGroup.Addon>
+              <FormControl type='text' placeholder='Amount owed' />
+            </InputGroup>
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col md={4}>
+            <Button type='Add to bill'>
+              Add to bill
+            </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    )
+  }
+
+export default AddBill
+
